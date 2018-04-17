608fd6150c3d843784f9fd76d10fc7a2eaa7eb0f
Juan David Garzon Alzate <juan.alzate@yuxiglobal.com>
2018-04-17 17:07:53 -0500

    fondo, validaciones

--- src/app/registro/card-registro/card-registro.component.ts
+++ src/app/registro/card-registro/card-registro.component.ts
@@ -1,8 +1,9 @@
 import { Usuario } from './../../modelo/Usuario';
-import { NgForm, FormGroup } from '@angular/forms';
+import { NgForm, FormGroup, FormGroupDirective } from '@angular/forms';
 import { RegistroService } from './../../servicios/registro.service';
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {FormBuilder, Validators, FormControl} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-card-registro',
@@ -10,7 +11,7 @@ import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
   styleUrls: ['./card-registro.component.css']
 })
 export class CardRegistroComponent implements OnInit {
-
+  matcher = new MyErrorStateMatcher();
   usuario: Usuario = new Usuario();
   registroForm: FormGroup;
 
@@ -32,5 +33,12 @@ export class CardRegistroComponent implements OnInit {
   onSubmit() {
     console.log(this.registroForm);
     this.registroService.insertUsuario(this.usuario);
+  }
+}
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
